7157f48f87f5e570d729e5e15859bc9652eaddc0
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-03-15 22:16:00 -0300

    instalada estrutura de pastas

--- App.js
+++ App.js
@@ -4,17 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Bem-vindo ao App Conversor!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
+  
 });

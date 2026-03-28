087f7bad62aaf4223c6da0150bce644a4a869192
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-03-28 15:48:02 -0300

    construindo card de resultado

--- src/components/ResultCard/index.js
+++ src/components/ResultCard/index.js
@@ -3,10 +3,13 @@ import { View, Text } from 'react-native';
 export function ResultCard({ amount, fromCurrency, toCurrency, exchangeRate, result, currencies }) {
     
     if(!result ||!exchangeRate) return null;
+    const toSymbol = currencies.find(c => c.code === toCurrency)?.symbol || toCurrency;
+    const fromSymbol = currencies.find(c => c.code === fromCurrency)?.symbol || fromCurrency;
+    
     return (
         <View >
             <Text >Resultado: </Text>
-            <Text>{result}</Text>
+            <Text>{toSymbol} {result}</Text>
             <Text >Taxa de cambio 1 {fromCurrency} = {exchangeRate.toFixed(4)} {toCurrency}: </Text>
         </View>
     );
